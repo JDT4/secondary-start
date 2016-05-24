9e84386cb8e72ca62a4a68c0a8e2ea4a1701b8bb
JDT4 <incipientviolet@gmail.com>
2016-05-24 16:10:53 +0100

    ready for front

--- app/scripts/CouncilControl.js
+++ app/scripts/CouncilControl.js
@@ -1,15 +1,21 @@
- 	angular
- 		.module('councilSpend')
- 		.controller('CouncilControl', ['$scope', 'apiCouncil', function ($scope, apiCouncil) {
- 			$scope.postcode = "RH80DX";
- 			$scope.update = function (code) {
- 				$scope.postcode = code;
- 			};
- 			apiCouncil.getTPDatabyPostCode($scope.postcode)
- 				.success(function (response) {
- 					$scope.councils = response;
- 				})
- 				.error(function (response) {
- 					alert("Please try a different postcode");
+ 		angular
+ 			.module('councilSpend')
+ 			.controller('CouncilControl', ['$scope', 'apiCouncil', function ($scope, apiCouncil) {
+ 				$scope.$watch('postcode', function () {
+ 					$scope.postcode = $scope.postcode.toUpperCase().replace(/\s+/g, '');
  				});
+ 				$scope.postcode = "SW1P3QL";
+ 				$scope.findData = function (code) {
+ 					$scope.loading = true;
+ 					apiCouncil.getTPDatabyPostCode(code)
+ 						.success(function (response) {
+ 							$scope.councils = response;
+ 							$scope.loading = false;
+ 						})
+ 						.error(function (response) {
+ 							alert("Please try a different postcode");
+ 							$scope.loading = false;
+ 						});
+
+ 				};
  		}]);
